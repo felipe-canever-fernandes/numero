4ec6cab7aafc17b4e578647b8ad7831dc3c8e955
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-12-04 19:15:31 -0300

    Create about menu item

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -31,13 +31,17 @@ namespace Numero
             this.counterLabel = new System.Windows.Forms.Label();
             this.plusButton = new System.Windows.Forms.Button();
             this.minusButton = new System.Windows.Forms.Button();
+            this.mainMenu = new System.Windows.Forms.MenuStrip();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mainMenu.SuspendLayout();
             this.SuspendLayout();
             // 
             // counterLabel
             // 
             this.counterLabel.Dock = System.Windows.Forms.DockStyle.Top;
             this.counterLabel.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.counterLabel.Location = new System.Drawing.Point(0, 0);
+            this.counterLabel.Location = new System.Drawing.Point(0, 24);
             this.counterLabel.Name = "counterLabel";
             this.counterLabel.Size = new System.Drawing.Size(181, 31);
             this.counterLabel.TabIndex = 0;
@@ -46,7 +50,7 @@ namespace Numero
             // 
             // plusButton
             // 
-            this.plusButton.Location = new System.Drawing.Point(93, 34);
+            this.plusButton.Location = new System.Drawing.Point(93, 58);
             this.plusButton.Name = "plusButton";
             this.plusButton.Size = new System.Drawing.Size(23, 23);
             this.plusButton.TabIndex = 1;
@@ -56,7 +60,7 @@ namespace Numero
             // 
             // minusButton
             // 
-            this.minusButton.Location = new System.Drawing.Point(64, 34);
+            this.minusButton.Location = new System.Drawing.Point(64, 58);
             this.minusButton.Name = "minusButton";
             this.minusButton.Size = new System.Drawing.Size(23, 23);
             this.minusButton.TabIndex = 2;
@@ -64,20 +68,49 @@ namespace Numero
             this.minusButton.UseVisualStyleBackColor = true;
             this.minusButton.Click += new System.EventHandler(this.minusButton_Click);
             // 
+            // mainMenu
+            // 
+            this.mainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem});
+            this.mainMenu.Location = new System.Drawing.Point(0, 0);
+            this.mainMenu.Name = "mainMenu";
+            this.mainMenu.Size = new System.Drawing.Size(181, 24);
+            this.mainMenu.TabIndex = 3;
+            this.mainMenu.Text = "menuStrip1";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
             // Main
             // 
             this.AccessibleName = "m";
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(181, 68);
+            this.ClientSize = new System.Drawing.Size(181, 93);
             this.Controls.Add(this.minusButton);
             this.Controls.Add(this.plusButton);
             this.Controls.Add(this.counterLabel);
+            this.Controls.Add(this.mainMenu);
+            this.MainMenuStrip = this.mainMenu;
             this.MaximizeBox = false;
             this.Name = "Main";
             this.Text = "Numero";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+            this.mainMenu.ResumeLayout(false);
+            this.mainMenu.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -86,6 +119,9 @@ namespace Numero
         private System.Windows.Forms.Label counterLabel;
         private System.Windows.Forms.Button plusButton;
         private System.Windows.Forms.Button minusButton;
+        private System.Windows.Forms.MenuStrip mainMenu;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
     }
 }
 
