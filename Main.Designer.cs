6b9b9140620325206d96c5fc1f5fa48e4b0a143f
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-12-04 16:18:52 -0300

    Create plus button

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -29,23 +29,34 @@ namespace Numero
         private void InitializeComponent()
         {
             this.counterLabel = new System.Windows.Forms.Label();
+            this.plusButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // counterLabel
             // 
             this.counterLabel.AutoSize = true;
             this.counterLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.counterLabel.Location = new System.Drawing.Point(128, 9);
+            this.counterLabel.Location = new System.Drawing.Point(12, 9);
             this.counterLabel.Name = "counterLabel";
             this.counterLabel.Size = new System.Drawing.Size(29, 31);
             this.counterLabel.TabIndex = 0;
             this.counterLabel.Text = "0";
             // 
+            // plusButton
+            // 
+            this.plusButton.Location = new System.Drawing.Point(47, 13);
+            this.plusButton.Name = "plusButton";
+            this.plusButton.Size = new System.Drawing.Size(23, 23);
+            this.plusButton.TabIndex = 1;
+            this.plusButton.Text = "+";
+            this.plusButton.UseVisualStyleBackColor = true;
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(196, 55);
+            this.Controls.Add(this.plusButton);
             this.Controls.Add(this.counterLabel);
             this.Name = "Main";
             this.Text = "Numero";
@@ -57,6 +68,7 @@ namespace Numero
         #endregion
 
         private System.Windows.Forms.Label counterLabel;
+        private System.Windows.Forms.Button plusButton;
     }
 }
 
